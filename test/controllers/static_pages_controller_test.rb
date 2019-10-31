9a298da0b3cd1b75bd518e649db3d7e186236290
soken <bknb.hy.lav@gmail.com>
2019-10-31 15:30:50 +0900

    ログイン画面追加（実装まだ）

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,20 +3,18 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
 
-  def setup
-    @base_title = "turistagram"
-  end
+ 
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Home | turistagram"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | turistagram"
   end
 
 end
