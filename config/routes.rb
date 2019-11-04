274d33cffeed72687d2ec8a6b7e455c7b3a881e6
soken <bknb.hy.lav@gmail.com>
2019-11-04 21:35:37 +0900

    新規登録追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get '/home' ,to:'static_pages#home'
   get '/about' ,to:'static_pages#about'
   get '/signup', to:'users#new'
-
+  
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   
