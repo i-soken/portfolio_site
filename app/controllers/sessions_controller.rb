10eab8eefc4274a72bf97daa9bf435d82516fb92
soken <bknb.hy.lav@gmail.com>
2019-11-10 20:09:52 +0900

    ログイン機能ほぼ完成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       flash[:success] = "ログインしました!"
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember user
       redirect_to user
     else
       flash.now[:danger] = "メールアドレスかパスワードが間違っています"
@@ -16,8 +18,10 @@ class SessionsController < ApplicationController
  end
 
  def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
  end
+
+
 
 end
